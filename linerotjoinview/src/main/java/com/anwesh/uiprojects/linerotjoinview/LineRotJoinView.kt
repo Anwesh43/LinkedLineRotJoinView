39a9a19165dcccd44d3956a201f02055dfc09ede
Anwesh43 <anweshthecool0@gmail.com>
2019-05-20 10:59:58 +0530

    adding LRJNode class

--- linerotjoinview/src/main/java/com/anwesh/uiprojects/linerotjoinview/LineRotJoinView.kt
+++ linerotjoinview/src/main/java/com/anwesh/uiprojects/linerotjoinview/LineRotJoinView.kt
@@ -124,4 +124,48 @@ class LineRotJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRJNode(var i : Int, val state : State = State()) {
+
+        private var prev : LRJNode? = null
+        private var next : LRJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LRJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRJNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRJNode {
+            var curr : LRJNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
